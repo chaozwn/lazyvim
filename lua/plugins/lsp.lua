4f23e19348b1a9cff0a73c46d978c006069586b8
jayce.zhao <326747337@qq.com>
2025-02-07 09:08:43 +0800

    feat: update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,31 +9,6 @@ return {
         update_in_insert = false,
         underline = true,
       },
-      -- servers = {
-      --   basedpyright = {
-      --     settings = {
-      --       basedpyright = {
-      --         analysis = {
-      --           typeCheckingMode = "basic",
-      --           autoImportCompletions = true,
-      --           autoSearchPaths = true,
-      --           diagnosticMode = "openFilesOnly",
-      --           useLibraryCodeForTypes = true,
-      --           reportMissingTypeStubs = false,
-      --           diagnosticSeverityOverrides = {
-      --             reportUnusedImport = "information",
-      --             reportUnusedFunction = "information",
-      --             reportUnusedVariable = "information",
-      --             reportGeneralTypeIssues = "none",
-      --             reportOptionalMemberAccess = "none",
-      --             reportOptionalSubscript = "none",
-      --             reportPrivateImportUsage = "none",
-      --           },
-      --         },
-      --       },
-      --     },
-      --   },
-      -- },
     },
   },
 }
