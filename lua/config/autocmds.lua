7aaf10298fb4bd4fd10b56be889e0e6e393430f4
jayce.zhao <326747337@qq.com>
2025-02-07 15:51:27 +0800

    feat: update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- make q close help, man , quickfix, dap floats
 vim.api.nvim_create_autocmd("BufWinEnter", {
   desc = "Make q close help, man, quickfix, dap floats",
   callback = function(args)
@@ -28,6 +30,15 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
         silent = true,
         nowait = true,
       })
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  desc = "Clean up q_close_windows cache",
+  callback = function(args)
+    if vim.g.q_close_windows then
+      vim.g.q_close_windows[args.buf] = nil
     end
   end,
 })
