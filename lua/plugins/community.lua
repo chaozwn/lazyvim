d2f0988689d43e175b9c932c55576cb20e2ac9e8
zhao_wei_nan <326747337@qq.com>
2025-03-02 10:21:28 +0800

    feat: update

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -5,4 +5,5 @@ return {
   { import = "lazyvim.plugins.extras.editor.refactoring" },
   { import = "lazyvim.plugins.extras.editor.overseer" },
   { import = "lazyvim.plugins.extras.test" },
+  { import = "lazyvim.plugins.extras.ai.copilot" },
 }
