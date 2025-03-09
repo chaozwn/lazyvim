735beb344206c24f61f5ac65f8358abfb8d81d6d
Liu, Jason JH <cattou147@icloud.com>
2025-03-09 21:26:45 +0800

    feat: change use copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -10,6 +10,7 @@ return {
         debounce = 150,
         keymap = {
           accept = "<C-;>", -- 接受建议的快捷键
+          accept_line = "<C-l>", -- 接受整行建议
           next = "<M-]>", -- 下一个建议
           prev = "<M-[>", -- 上一个建议
           dismiss = "<C-]>", -- 关闭建议
