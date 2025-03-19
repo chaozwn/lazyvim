0e6ee5d985db49ccc224a385c11fe87849d2fbb4
zhao_wei_nan <326747337@qq.com>
2025-03-19 14:16:38 +0800

    feat: update

---
+++ lua/plugins/git-blame.lua
@@ -0,0 +1,19 @@
+return {
+  "f-person/git-blame.nvim",
+  event = "LazyFile",
+  cmd = {
+    "GitBlameToggle",
+    "GitBlameEnable",
+    "GitBlameOpenCommitURL",
+    "GitBlameCopyCommitURL",
+    "GitBlameOpenFileURL",
+    "GitBlameCopyFileURL",
+    "GitBlameCopySHA",
+  },
+  opts = {
+    enabled = true,
+    date_format = "%r",
+    message_template = "  <author> 󰔠 <date> 󰈚 <summary>  <sha>",
+    message_when_not_committed = "  Not Committed Yet",
+  },
+}
