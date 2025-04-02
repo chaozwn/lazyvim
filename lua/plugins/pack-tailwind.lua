154bdbad83c517ab19c81bebfa44e73b9b49b464
zhao_wei_nan <326747337@qq.com>
2025-04-02 15:12:02 +0800

    feat: update

--- lua/plugins/pack-tailwind.lua
+++ lua/plugins/pack-tailwind.lua
@@ -14,5 +14,51 @@ return {
     })
   end,
   { import = "lazyvim.plugins.extras.lang.tailwind" },
-  {},
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          settings = {
+            tailwindCSS = {
+              classFunctions = {
+                "cva",
+                "cx",
+                "tw",
+                "clsx",
+              },
+            },
+          },
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = LazyVim.lsp.get_raw_config("tailwindcss")
+          opts.filetypes = opts.filetypes or {}
+
+          -- Add default filetypes
+          vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+          -- Remove excluded filetypes
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, opts.filetypes)
+
+          opts.settings = vim.tbl_deep_extend("force", opts.settings, {
+            tailwindCSS = {
+              includeLanguages = {
+                elixir = "html-eex",
+                eelixir = "html-eex",
+                heex = "html-eex",
+              },
+            },
+          })
+          -- Add additional filetypes
+          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+          require("snacks").debug(opts.settings)
+        end,
+      },
+    },
+  },
 }
