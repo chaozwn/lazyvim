bd31e2f1df12cdf1921cbcd4c5073121bff9dd69
zhao_wei_nan <326747337@qq.com>
2025-04-26 14:05:31 +0800

    feat: remove useless plugins

--- lua/plugins/rainbow-delimiter.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-return {
-  "HiPhish/rainbow-delimiters.nvim",
-  enabled = false,
-  dependencies = "nvim-treesitter/nvim-treesitter",
-  event = "LazyFile",
-  main = "rainbow-delimiters.setup",
-}
